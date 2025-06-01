0bd2e40ac497da989adb501df1768cd73b38cb16
senderdvir <senderdvir@gmail.com>
2025-06-02 02:23:39 +0300

    Update config.py

--- src/config.py
+++ src/config.py
@@ -1,13 +1,3 @@
-import os
-
-POSTGRES_CONFIG = {
-    "host": os.getenv("POSTGRES_HOST", "localhost"),
-    "port": int(os.getenv("POSTGRES_PORT", 5432)),
-    "datavase": os.getenv("POSTGRES_DATABASE", "launches"),
-    "user": os.getenv("POSTGRES_USER", "spacex"),
-    "passowrd": os.getenv("POSRGRES_PASSWORD", "spacex")
-}
-
 SPACEX_API_URL_LAUNCHES = "https://api.spacexdata.com/v5/launches"
 SPACEX_API_URL_PAYLOADS = "https://api.spacexdata.com/v4/payloads"
 SPACEX_API_URL_LAUNCHPAD = "https://api.spacexdata.com/v4/launchpads"
